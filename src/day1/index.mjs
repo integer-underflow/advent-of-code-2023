9f5cbeee22c6df0637877aab3ea0bc42b8f21042
integer-underflow <2921893+integer-underflow@users.noreply.github.com>
2023-12-03 14:24:33 +0700

    refactor(day1): cleanup, rename

--- src/day1/index.mjs
+++ src/day1/index.mjs
@@ -1,35 +1,32 @@
-import { compose, flatten, head, isNil, map, split, sum, last, join, when, test, unless, curry } from "ramda"
+import { compose, flatten, head, isNil, map, split, sum, last, join, test, unless, curry } from "ramda"
 import { INPUT } from "./input.mjs"
 
-const log = x => {
-    console.log(x)
-    return x
+const log = arg => {
+    console.log(arg)
+    return arg
 }
 
-const matchAll            = curry((regex, str) => [...str.matchAll(regex)])
-const splitLines          = split("\n")
-const isDigit             = test(/\d/)
-const matchOneDigitOrWord = matchAll(/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/g)
-const firstDigit          = compose(unless(isNil, last), head, matchOneDigitOrWord)
-const lastDigit           = compose(unless(isNil, last), last, matchOneDigitOrWord)
-const firstAndLastDigit   = str => [firstDigit(str), lastDigit(str)]
-const wordToDigit         = word => ({
-    one: '1',
-    two: '2',
-    three: '3',
-    four: '4',
-    five: '5',
-    six: '6',
-    seven: '7',
-    eight: '8',
-    nine: '9'
-}[word] ?? '0')
+const matchAll                         = curry((regex, str) => [...str.matchAll(regex)])
+const splitLines                       = split("\n")
+const hasDigitCharacter                = test(/\d/)
+const matchAllDigitOrDigitWord         = matchAll(/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/g)
+const findFirstDigit                   = compose(unless(isNil, last), head, matchAllDigitOrDigitWord)
+const findLastDigit                    = compose(unless(isNil, last), last, matchAllDigitOrDigitWord)
+const findFirstAndLastDigit            = str => [findFirstDigit(str), findLastDigit(str)]
+const convertDigitWordToDigitCharacter = digitWord => ({
+    one: '1', two: '2', three: '3', four: '4', five: '5', six: '6', seven: '7', eight: '8', nine: '9'
+}[digitWord] ?? '0')
 
 const result = compose(
     sum,
     flatten,
-    map(compose(parseInt, join(''), map(unless(isDigit, wordToDigit)), firstAndLastDigit)), 
+    map(compose(
+        parseInt, 
+        join(''), 
+        map(unless(hasDigitCharacter, convertDigitWordToDigitCharacter)), 
+        findFirstAndLastDigit
+    )), 
     splitLines
 )(INPUT)
 
-console.log(result)
+log(result)
